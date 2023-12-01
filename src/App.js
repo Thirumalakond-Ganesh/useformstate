2e69f835c4c614d3732837cbe031ef2253ad2986
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-12-01 11:53:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm, useFormState, FormProvider } from "react-hook-form";
+import './App.css'
+const MyForm = () => {
+  const methods = useForm();
+  const { handleSubmit, register } = methods;
+  const { dirty, isSubmitting, isValidating, submitCount } = useFormState();
 
-function App() {
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="inputA">Input A</label>
+      <input {...register("inputA", { required: true })} />
+
+      <label htmlFor="inputB">Input B</label>
+      <input {...register("inputB", { required: true })} />
+
+      <p>Form State:</p>
+      <ul>
+      <li>Dirty: {dirty !== undefined ? dirty.toString() : "N/A"}</li>
+        <li>Is Submitting: {isSubmitting !== undefined ? isSubmitting.toString() : "N/A"}</li>
+        <li>Is Validating: {isValidating !== undefined ? isValidating.toString() : "N/A"}</li>
+        <li>Submit Count: {submitCount !== undefined ? submitCount : "N/A"}</li>
+      </ul>
+
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
+
+const App = () => {
+  const methods = useForm();
+
+  return (
+    <FormProvider {...methods}>
+      <MyForm />
+    </FormProvider>
+  );
+};
 
 export default App;
